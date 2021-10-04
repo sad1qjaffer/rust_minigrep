11e6d8456ba036d9b5b3ee5d3c6c5d4a80059a44
sad1qjaffer <sadiqjaffer@outlook.com>
2021-10-04 18:20:28 +0100

    Added lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use rust_minigrep::Config;
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,30 +15,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = rust_minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config {query, filename})
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
-    Ok(())
 }
